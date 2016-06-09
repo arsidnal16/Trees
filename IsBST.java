4975e2b2b87c664dcdf9c89a567b8f7e77eb9e80
Siddhartha Singh <singh.si@husky.neu.edu>
2016-06-09 13:00:09 -0400

    Add files via upload
    To check if a given tree is a binary search tree or not.

---
+++ IsBST.java
@@ -0,0 +1,40 @@
+package Trees;
+
+public class IsBST {
+	
+	// A program to find out if the given tree is a binary search tree.
+	
+	Node root;
+	Node previous = null;
+	public boolean isBST(Node root){ // A function to check if tree is BST, returns true if tree is BST
+	
+		if(root!=null){
+			if(!isBST(root.left)){
+				return false;
+			}
+			if(previous != null && root.data <= previous.data){
+				return false;
+			}
+			previous = root;
+			return isBST(root.right);
+		}
+		return true;
+	}
+	
+	public static void main(String args[])
+    {
+        IsBST tree = new IsBST();
+        tree.root = new Node(4);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(5);
+        tree.root.left.left = new Node(1);
+        tree.root.left.right = new Node(3);
+ 
+        if (tree.isBST(tree.root))
+            System.out.println("It is a binary search tree");
+        else
+            System.out.println("It is not a binary search tree");
+    }
+}
+
+
